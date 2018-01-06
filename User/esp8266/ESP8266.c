c375cd24bf8df67b3fd730a13428b4063c748567
dingxu6207 <32772701+dingxu6207@users.noreply.github.com>
2018-01-06 21:45:28 +0800

    删除无用代码

--- User/esp8266/ESP8266.c
+++ User/esp8266/ESP8266.c
@@ -5,24 +5,11 @@
 
 void ESP8266_Set(unsigned char *puf) // 数组指针*puf指向字符串数组  
 {
-	#if 0
-	WifiUsart_SendString(USART3, puf);
-	Usart_SendString(USART1, puf);
-	Delay_us(5);
-
-	WifiUsart_SendByte(USART3, '\r');
-	Usart_SendByte(USART1, '\r');
-    Delay_us(5);
 	
-	WifiUsart_SendByte(USART3, '\n');
-	Usart_SendByte(USART1, '\n');
-    Delay_ms(1000);	
-	#endif
-
 	  while(*puf!='\0')    //遇到空格跳出循环
       {
            WifiUsart_SendByte(USART3, *puf);
-		   Usart_SendByte(USART1, *puf);
+					 Usart_SendByte(USART1, *puf);
            Delay_us(5);
            puf++;      
       }
